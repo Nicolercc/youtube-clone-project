19d4574693846f42e967cedc5b48b4a732e4beec
Amina-Moufakkir <aminamouf22@gmail.com>
2023-05-29 16:03:04 -0400

    fixing the video title

--- src/Components/VideoCard.jsx
+++ src/Components/VideoCard.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 const VideoCard = ({ video }) => {
   const thumbnail =
     video.snippet.thumbnails?.medium?.url ||
-    "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=";
+    'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=';
 
   const publishedDate = new Date(video.snippet.publishedAt);
   const currentDate = new Date();
@@ -39,7 +39,7 @@ const VideoCard = ({ video }) => {
     <div className="video-card">
       <Link to={`/video/${video.id}`}>
         <img src={thumbnail} alt="thumbnail" className="thumbnail" />
-        <h2 className="video-title">{video.snippet.title}</h2>
+        <h2 className="video-title">{video.snippet.title.slice(0, 60)}</h2>
       </Link>
       <p className="channel">{video.snippet.channelTitle}</p>
       <span className="channel">
